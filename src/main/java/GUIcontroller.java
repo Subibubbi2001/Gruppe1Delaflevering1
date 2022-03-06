1a49bd740d2bf0699456c0924a1212f359539e7a
Subhaan <s215852@dtu.dk>
2022-03-06 14:37:23 +0100

    Initial commit

---
+++ src/main/java/GUIcontroller.java
@@ -0,0 +1,11 @@
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class GUIcontroller {
+    public Label label2;
+
+    public void buttonclicked(MouseEvent mouseEvent) {
+        label2.setText("Brugernavn og adgangskode");
+    }
+}
+
